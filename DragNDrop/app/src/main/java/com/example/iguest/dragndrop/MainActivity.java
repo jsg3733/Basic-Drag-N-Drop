520b0f99a7307af7f46690d9182ffdf8a735a575
Jacob Gilmore <jsg3733@uw.edu>
2015-06-01 19:59:29 -0700

    Basic Drag N Drop functionality gotten from William J. Francis at http://www.techrepublic.com/blog/software-engineer/try-androids-useful-drag-and-drop-api/.

---
+++ DragNDrop/app/src/main/java/com/example/iguest/dragndrop/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.iguest.dragndrop;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnTouchListener, View.OnDragListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.red_ball).setOnTouchListener(this);
+        findViewById(R.id.green_ball).setOnTouchListener(this);
+        findViewById(R.id.blue_ball).setOnTouchListener(this);
+        findViewById(R.id.top_container).setOnDragListener(this);
+        findViewById(R.id.bottom_container).setOnDragListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent e) {
+        if (e.getAction() == MotionEvent.ACTION_DOWN) {
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(null, shadowBuilder, v, 0);
+            v.setVisibility(View.INVISIBLE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent e) {
+        if (e.getAction()==DragEvent.ACTION_DROP) {
+            View view = (View) e.getLocalState();
+            ViewGroup from = (ViewGroup) view.getParent();
+            from.removeView(view);
+            LinearLayout to = (LinearLayout) v;
+            to.addView(view);
+            view.setVisibility(View.VISIBLE);
+        }
+        return true;
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
